359a301f3d730a9912a400cf5ee539f9f1c139a1
Valentin Décaillet <decaillet@icare.ch>
2023-02-24 18:55:37 +0100

    feat(evaluator): write orchestrator with placeholders

--- src/evaluator.py
+++ src/evaluator.py
@@ -1,7 +1,32 @@
 class Evaluator():
     def __init__(self):
-        self.mae = 0
+        pass
 
     def evaluate(self):
-        self.mae = 42
-        return self.mae
+        self._prepare_data()
+
+        self._preprocess_data()
+
+        self._train_model()
+
+        return self._evaluate_model()
+
+    # region placeholder private methods # TODO remove when corresponding IoD is implemented
+
+    @classmethod
+    def _prepare_data(cls):
+        pass
+
+    @classmethod
+    def _preprocess_data(cls):
+        pass
+
+    @classmethod
+    def _train_model(cls):
+        pass
+
+    @classmethod
+    def _evaluate_model(cls):
+        return 42
+
+    # endregion placeholder private methods
