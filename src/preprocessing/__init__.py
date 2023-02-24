c8aae78ed21cef78e0ca60addc89196dc63959ad
Valentin Décaillet <decaillet@icare.ch>
2023-02-25 00:45:33 +0100

    feat(preprocessor): add min-max preprocessor

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -2,3 +2,4 @@ from .contract import Preprocessor  # isort: skip (contract must be first)
 
 from .dummy_preprocessor import DummyPreprocessor
 from .factory import PreprocessorFactory
+from .min_max_preprocessor import MinMaxPreprocessor
