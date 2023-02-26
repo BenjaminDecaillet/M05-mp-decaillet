3b378cc7ca80c177ae92e79b9804a2583d328f52
Valentin Décaillet <decaillv@gmail.com>
2023-02-26 20:34:25 +0100

    test(service): patch dependencies

--- src/service.py
+++ src/service.py
@@ -1,13 +1,12 @@
-from src import Evaluator
-from src.preprocessing import PreprocessorFactory
+import src.preprocessing
 
 
 class Service:
     def __init__(self) -> None:
-        self._preprocessor_factory = PreprocessorFactory("dummy")
+        self._preprocessor_factory = src.preprocessing.PreprocessorFactory("dummy")
 
     def run(self) -> None:
-        evaluator = Evaluator(self._preprocessor_factory.create())
+        evaluator = src.Evaluator(self._preprocessor_factory.create())
         mae = evaluator.evaluate()
 
         print(f"MAE: {mae}")
