7df0dcc1df033921fd89b8d8a15ef1801219008e
Valentin Décaillet <decaillv@gmail.com>
2023-03-05 20:19:01 +0100

    feat(evaluator): evaluate multiple times

--- src/evaluator.py
+++ src/evaluator.py
@@ -1,3 +1,5 @@
+from random import randint
+
 import pandas as pd
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
@@ -9,7 +11,8 @@ from src.preprocessing import Preprocessor
 class Evaluator():
     def __init__(self,
                  preprocessor: Preprocessor,
-                 estimator: Estimator):
+                 estimator: Estimator,
+                 evaluation_count: int):
         if not isinstance(preprocessor, Preprocessor):
             raise TypeError("preprocessor must be a Preprocessor")
         self._preprocessor = preprocessor
@@ -18,10 +21,16 @@ class Evaluator():
             raise TypeError("estimator must be an Estimator")
         self._estimator = estimator
 
+        if not isinstance(evaluation_count, int):
+            raise TypeError("evaluation_count must be an int")
+        if not evaluation_count >= 1:
+            raise ValueError("evaluation_count must be >= 1")
+        self._evaluation_count = evaluation_count
+
     def evaluate(self):
         dataset = self._prepare_data()
-
-        return self._evaluate_once(**dataset)
+        mean_absolute_errors = [self._evaluate_once(**dataset) for _ in range(self._evaluation_count)]
+        return sum(mean_absolute_errors) / len(mean_absolute_errors)
 
     def _evaluate_once(self, dataset: pd.DataFrame, features_names: list[str], targets_names: list[str]) -> float:
         """Trains and evaluates the model once.
@@ -33,7 +42,7 @@ class Evaluator():
         assert isinstance(features_names, list) and all(name in dataset.columns for name in features_names)
         assert isinstance(targets_names, list) and all(name in dataset.columns for name in targets_names)
 
-        training_set, test_set = train_test_split(dataset, test_size=0.5)
+        training_set, test_set = train_test_split(dataset, test_size=0.5, random_state=randint(0, 1000))
 
         preprocessed_training_features = self._preprocessor.fit_transform(training_set[features_names])
         preprocessed_test_features = self._preprocessor.transform(test_set[features_names])
--- unit_tests/test_evaluator.py
+++ unit_tests/test_evaluator.py
@@ -10,6 +10,7 @@ from src.preprocessing import Preprocessor
 class TestEvaluator(unittest.TestCase):
     def setUp(self):
         self.addCleanup(unittest.mock.patch.stopall)
+        self._randint_patch = unittest.mock.patch("src.evaluator.randint").start()
         self._tts_patch = unittest.mock.patch("src.evaluator.train_test_split").start()
         self._mae_patch = unittest.mock.patch("src.evaluator.mean_absolute_error").start()
 
@@ -18,31 +19,42 @@ class TestEvaluator(unittest.TestCase):
 
     def test__can_init(self):
         evaluator = Evaluator(self._mock_preprocessor,
-                              self._mock_estimator)
+                              self._mock_estimator,
+                              evaluation_count=3)
 
         self.assertIsNotNone(evaluator)
 
     def test__init_fails__on_bad_preprocessor(self):
         with self.assertRaisesRegex(TypeError, "^preprocessor must be a Preprocessor$"):
-            Evaluator("not a preprocessor", self._mock_estimator)
+            Evaluator("not a preprocessor", self._mock_estimator, 2)
 
     def test__init_fails__on_bad_estimator(self):
         with self.assertRaisesRegex(TypeError, "^estimator must be an Estimator$"):
-            Evaluator(self._mock_preprocessor, "not an estimator")
+            Evaluator(self._mock_preprocessor, "not an estimator", 1)
+
+    def test__init_fails__on_bad_evaluation_count(self):
+        with self.assertRaisesRegex(TypeError, "^evaluation_count must be an int$"):
+            Evaluator(self._mock_preprocessor, self._mock_estimator, None)  # None is not an int
+
+        with self.assertRaisesRegex(ValueError, "^evaluation_count must be >= 1$"):
+            Evaluator(self._mock_preprocessor, self._mock_estimator, 0)
 
     def test__can_evaluate(self):
         evaluator = Evaluator(self._mock_preprocessor,
-                              self._mock_estimator)
+                              self._mock_estimator,
+                              evaluation_count=3)
         self._tts_patch.return_value = (unittest.mock.MagicMock(spec=pd.DataFrame),
                                         unittest.mock.MagicMock(spec=pd.DataFrame))
-        self._mae_patch.return_value = 42
+        self._mae_patch.side_effect = [40.0, 43.0, 43.0]
 
         result = evaluator.evaluate()
 
-        self.assertEqual(result, 42)
-        self._tts_patch.assert_called_once()
-        self._mock_preprocessor.fit_transform.assert_called_once()
-        self._mock_preprocessor.transform.assert_called_once()
-        self._mock_estimator.fit.assert_called_once()
-        self._mock_estimator.predict.assert_called_once()
-        self._mae_patch.assert_called_once()
+        self.assertEqual(result, 42.0)
+
+        self.assertEqual(self._randint_patch.call_count, 3)
+        self.assertEqual(self._tts_patch.call_count, 3)
+        self.assertEqual(self._mock_preprocessor.fit_transform.call_count, 3)
+        self.assertEqual(self._mock_preprocessor.transform.call_count, 3)
+        self.assertEqual(self._mock_estimator.fit.call_count, 3)
+        self.assertEqual(self._mock_estimator.predict.call_count, 3)
+        self.assertEqual(self._mae_patch.call_count, 3)
