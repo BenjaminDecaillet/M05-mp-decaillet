109229f6016319b567d7705c2af23d536c331fcf
Valentin Décaillet <decaillv@gmail.com>
2023-03-05 11:22:21 +0100

    feat(estimator): use dummy-estimator

--- src/evaluator.py
+++ src/evaluator.py
@@ -1,23 +1,34 @@
 import pandas as pd
 
+from src.estimating import Estimator
 from src.preprocessing import Preprocessor
 
 
 class Evaluator():
-    def __init__(self, preprocessor: Preprocessor):
+    def __init__(self,
+                 preprocessor: Preprocessor,
+                 estimator: Estimator):
         if not isinstance(preprocessor, Preprocessor):
             raise TypeError("preprocessor must be a Preprocessor")
         self._preprocessor = preprocessor
+
+        if not isinstance(estimator, Estimator):
+            raise TypeError("estimator must be an Estimator")
+        self._estimator = estimator
 
     def evaluate(self):
         self._prepare_data()
 
         training_features = pd.DataFrame(data=[1], columns=["foo"])
+        training_targets = pd.DataFrame(data=[1], columns=["bar"])
         test_features = pd.DataFrame(data=[2], columns=["foo"])
-        _ = self._preprocessor.fit_transform(training_features)
-        _ = self._preprocessor.transform(test_features)
+        test_targets = pd.DataFrame(data=[2], columns=["bar"])
 
-        self._train_model()
+        preprocessed_training_features = self._preprocessor.fit_transform(training_features)
+        preprocessed_test_features = self._preprocessor.transform(test_features)
+
+        self._estimator.fit(preprocessed_training_features, training_targets)
+        predictions = self._estimator.predict(preprocessed_test_features)
 
         return self._evaluate_model()
 
@@ -25,10 +36,6 @@ class Evaluator():
 
     @classmethod
     def _prepare_data(cls):
-        pass
-
-    @classmethod
-    def _train_model(cls):
         pass
 
     @classmethod
--- unit_tests/test_evaluator.py
+++ unit_tests/test_evaluator.py
@@ -1,27 +1,37 @@
 import unittest.mock
 
 from src import Evaluator
+from src.estimating import Estimator
 from src.preprocessing import Preprocessor
 
 
 class TestEvaluator(unittest.TestCase):
     def setUp(self):
         self._mock_preprocessor = unittest.mock.Mock(spec=Preprocessor)
+        self._mock_estimator = unittest.mock.Mock(spec=Estimator)
 
     def test__can_init(self):
-        evaluator = Evaluator(self._mock_preprocessor)
+        evaluator = Evaluator(self._mock_preprocessor,
+                              self._mock_estimator)
 
         self.assertIsNotNone(evaluator)
 
     def test__init_fails__on_bad_preprocessor(self):
         with self.assertRaisesRegex(TypeError, "^preprocessor must be a Preprocessor$"):
-            Evaluator("not a preprocessor")
+            Evaluator("not a preprocessor", self._mock_estimator)
+
+    def test__init_fails__on_bad_estimator(self):
+        with self.assertRaisesRegex(TypeError, "^estimator must be an Estimator$"):
+            Evaluator(self._mock_preprocessor, "not an estimator")
 
     def test__can_evaluate(self):
-        evaluator = Evaluator(self._mock_preprocessor)
+        evaluator = Evaluator(self._mock_preprocessor,
+                              self._mock_estimator)
 
         result = evaluator.evaluate()
 
         self.assertEqual(result, 42)
         self._mock_preprocessor.fit_transform.assert_called_once()
         self._mock_preprocessor.transform.assert_called_once()
+        self._mock_estimator.fit.assert_called_once()
+        self._mock_estimator.predict.assert_called_once()
