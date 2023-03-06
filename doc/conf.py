f617b6c97bbe8af80a34a2d9c522ca8c3826d052
Valentin Décaillet <decaillv@gmail.com>
2023-03-06 18:14:28 +0100

    doc(sphinx): add sphinx

---
+++ doc/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'M05-mp-decaillet'
+copyright = '2023, Benjamin Décaillet, Valentin Décaillet'
+author = 'Benjamin Décaillet, Valentin Décaillet'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ doc/index.rst
@@ -0,0 +1,20 @@
+.. M05-mp-decaillet documentation master file, created by
+   sphinx-quickstart on Mon Mar  6 18:11:53 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to M05-mp-decaillet's documentation!
+============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
