3493c1934c55e5416525d0d010dd54c839d540de
Valentin Décaillet <decaillv@gmail.com>
2023-03-06 23:28:22 +0100

    doc(logo): add logo

--- doc/conf.py
+++ doc/conf.py
@@ -32,3 +32,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = 'img/logo.png'
---
+++ doc/img/info.md
@@ -0,0 +1,5 @@
+Source of images
+----------------
+
+[logo.png](logo.png) generated with [DALL-E](https://labs.openai.com/);
+prompt "a clipart of a city skyline, but the buildings are bottles of wine"
