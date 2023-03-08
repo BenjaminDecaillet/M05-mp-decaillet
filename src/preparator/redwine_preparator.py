12c1b787c827c59c4c513f7b901f6e646990be98
BenjaminDecaillet <decaillet.benjamin@gmail.com>
2023-03-08 14:52:08 +0100

    test(red-wine): cover

--- src/preparator/redwine_preparator.py
+++ src/preparator/redwine_preparator.py
@@ -3,7 +3,7 @@ import pandas as pd
 from src.preparator import Preparator
 
 
-class RedWinePreparator(Preparator):  # pragma: no cover TODO add unit tests
+class RedWinePreparator(Preparator):
     def __init__(self, source: str):
         if source == 'file':
             self._source = 'data/winequality-red.csv'
@@ -11,18 +11,8 @@ class RedWinePreparator(Preparator):  # pragma: no cover TODO add unit tests
             self._source = 'https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
 
     def load_data(self) -> pd.DataFrame:
-        features_names = [
-            'fixed acidity',
-            'volatile acidity',
-            'citric acid',
-            'residual sugar',
-            'chlorides',
-            'free sulfur dioxide',
-            'total sulfur dioxide',
-            'density',
-            'pH',
-            'sulphates',
-            'alcohol']
+        features_names = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides',
+                          'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']
         targets_names = ['quality']
         data = pd.read_csv(self._source, sep=';')
         dataset = {"dataset": data, "features_names": features_names, "targets_names": targets_names}
