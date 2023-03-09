c09537b0de09917637d79182a24d3f20afde43f2
Benjamin Décaillet <benjamin.decaillet@synchrotech.ch>
2023-03-09 11:22:16 +0100

    feat(estimator): add sklearn estimator base

--- src/estimating/__init__.py
+++ src/estimating/__init__.py
@@ -1,4 +1,5 @@
 from .contract import Estimator  # isort: skip (contract must be first)
+from .sklearn_estimator_base import SkLearnEstimatorBase  # isort: skip (import base class before its children)
 
 from .factory import EstimatorFactory
 from .linear_estimator import LinearEstimator
--- src/estimating/regression_estimator.py
+++ src/estimating/regression_estimator.py
@@ -1,20 +1,8 @@
-import numpy as np
-import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 
-from src.estimating import Estimator
+from src.estimating import SkLearnEstimatorBase
 
 
-class RegressionEstimator(Estimator):
-    def __init__(self):
-        super().__init__()
-        self._model = DecisionTreeRegressor(max_depth=7, random_state=42)
-
-    def fit(self, features: pd.DataFrame, targets: pd.DataFrame):
-        """ Performs grid search over the 'max_depth' parameter for a
-        decision tree regressor trained on the input data [features, targets]. """
-
-        self._model.fit(features, targets)
-
-    def predict(self, features: pd.DataFrame) -> np.array:
-        return self._model.predict(features)
+class RegressionEstimator(SkLearnEstimatorBase):
+    def _get_model(self):
+        return DecisionTreeRegressor(max_depth=7, random_state=42)
