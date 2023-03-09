b49ec6389785c3be5fecedfd82c853b161bc6b9e
Valentin Décaillet <decaillv@gmail.com>
2023-03-10 00:22:45 +0100

    style(wines): DRY wine preparators

--- src/preparator/__init__.py
+++ src/preparator/__init__.py
@@ -3,6 +3,5 @@ from .base_preparator import BasePreparator  # isort: skip (import base class be
 
 from .boston_preparator import BostonPreparator
 from .factory import PreparatorFactory
-from .redwine_preparator import RedWinePreparator
-from .whitewine_preparator import WhiteWinePreparator
-from .wine_preparator import WinePreparator
+from .wine_preparator import (RedWinePreparator, WhiteWinePreparator,
+                              WinePreparator)
--- src/preparator/wine_preparator.py
+++ src/preparator/wine_preparator.py
@@ -1,18 +1,38 @@
 from src.preparator import BasePreparator
 
 
-class WinePreparator(BasePreparator):
-    def __init__(self, source: str):
+class _BaseWinePreparator(BasePreparator):
+    def __init__(self, source: str, include_red: bool, include_white: bool):
+        assert include_red or include_white
+
         sources = []
         if source == 'file':
-            sources.append('data/winequality-red.csv')
-            sources.append('data/winequality-white.csv')
+            if include_red:
+                sources.append('data/winequality-red.csv')
+            if include_white:
+                sources.append('data/winequality-white.csv')
         else:
-            sources.append(
-                'https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv')
-            sources.append(
-                'https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv')
+            if include_red:
+                sources.append('https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv')
+            if include_white:
+                sources.append('https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv')
+
         features_names = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides',
                           'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']
         targets_names = ['quality']
         super().__init__(sources, features_names, targets_names, sep=';')
+
+
+class RedWinePreparator(_BaseWinePreparator):
+    def __init__(self, source: str):
+        super().__init__(source, include_red=True, include_white=False)
+
+
+class WhiteWinePreparator(_BaseWinePreparator):
+    def __init__(self, source: str):
+        super().__init__(source, include_red=False, include_white=True)
+
+
+class WinePreparator(_BaseWinePreparator):
+    def __init__(self, source: str):
+        super().__init__(source, include_red=True, include_white=True)
