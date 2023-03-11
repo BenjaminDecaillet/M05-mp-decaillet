304b0070ffcfad7eb779a657e7525bec202a1b77
BenjaminDecaillet <decaillet.benjamin@gmail.com>
2023-03-11 10:42:56 +0100

    feat(linear-estimator): add LR estimator

--- src/estimating/__init__.py
+++ src/estimating/__init__.py
@@ -2,3 +2,4 @@ from .contract import Estimator  # isort: skip (contract must be first)
 
 from .dummy_estimator import DummyEstimator
 from .factory import EstimatorFactory
+from .linear_estimator import LinearEstimator
