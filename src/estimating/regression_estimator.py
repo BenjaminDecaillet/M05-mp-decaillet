838dfb1608131b92b2359c60e4c736f28db0547d
BenjaminDecaillet <decaillet.benjamin@gmail.com>
2023-03-11 10:43:39 +0100

    test(regression-estimator): cover

--- src/estimating/regression_estimator.py
+++ src/estimating/regression_estimator.py
@@ -5,13 +5,13 @@ from sklearn.tree import DecisionTreeRegressor
 from src.estimating import Estimator
 
 
-class RegressionEstimator(Estimator):  # pragma: no cover TODO add unit tests + define if we want to pass the max_depth
+class RegressionEstimator(Estimator):
     def __init__(self):
         super().__init__()
         self._model = DecisionTreeRegressor(max_depth=7, random_state=42)
 
     def fit(self, features: pd.DataFrame, targets: pd.DataFrame):
-        """ Performs grid search over the 'max_depth' parameter for a 
+        """ Performs grid search over the 'max_depth' parameter for a
         decision tree regressor trained on the input data [features, targets]. """
 
         self._model.fit(features, targets)
---
+++ unit_tests/estimating/test_regression_estimator.py
@@ -0,0 +1,27 @@
+import unittest
+
+import numpy as np
+import pandas as pd
+
+from src.estimating import RegressionEstimator
+
+
+class TestRegressionEstimator(unittest.TestCase):
+    def test_fit(self):
+        estimator = RegressionEstimator()
+        features = pd.DataFrame({'x1': [1, 2, 3], 'x2': [4, 5, 6]})
+        targets = pd.Series([7, 8, 9])
+
+        self.assertIsNone(estimator.fit(features, targets))
+
+    def test_predict(self):
+        estimator = RegressionEstimator()
+        features = pd.DataFrame({'x1': [1, 2, 3], 'x2': [4, 5, 6]})
+        targets = pd.Series([7, 8, 9])
+        test_features = pd.DataFrame({'x1': [4, 5, 6], 'x2': [7, 8, 9]})
+
+        estimator.fit(features, targets)
+        result = estimator.predict(test_features)
+
+        self.assertEqual(result.shape, (3,))
+        np.testing.assert_array_almost_equal(result, np.array([9., 9., 9.]))
