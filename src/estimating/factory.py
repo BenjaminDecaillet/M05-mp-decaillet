91ff3043855941ff936bbc66452dbcea6e1f2db0
BenjaminDecaillet <decaillet.benjamin@gmail.com>
2023-03-11 10:42:56 +0100

    feat(estimator): remove dummy

--- src/estimating/factory.py
+++ src/estimating/factory.py
@@ -4,12 +4,10 @@ from src.estimating import Estimator
 
 class EstimatorFactory:
     def __init__(self, type: str):
-        if type not in {"dummy", "linear"}:
+        if type not in {"linear"}:
             raise ValueError(f"Unknown estimator type '{type}'")
         self._type = type
 
     def create(self) -> Estimator:
-        if self._type == "dummy":
-            return src.estimating.DummyEstimator()
         if self._type == "linear":
             return src.estimating.LinearEstimator()
--- unit_tests/estimating/test_factory.py
+++ unit_tests/estimating/test_factory.py
@@ -4,18 +4,18 @@ from src.estimating import EstimatorFactory
 
 
 class TestEstimatorFactory(unittest.TestCase):
-    def test__can_init_dummy(self):
-        estimator_factory = EstimatorFactory("dummy")
+    def test__can_init_linear(self):
+        estimator_factory = EstimatorFactory("linear")
         self.assertIsNotNone(estimator_factory)
 
     def test__init_fails__on_bad_type(self):
         with self.assertRaisesRegex(ValueError, "^Unknown estimator type 'foo'$"):
             EstimatorFactory("foo")
 
-    def test__can_create_dummy(self):
-        estimator_factory = EstimatorFactory("dummy")
+    def test__can_create_linear(self):
+        estimator_factory = EstimatorFactory("linear")
 
-        with unittest.mock.patch("src.estimating.DummyEstimator") as mock:
+        with unittest.mock.patch("src.estimating.LinearEstimator") as mock:
             estimator = estimator_factory.create()
 
         mock.assert_called_once_with()
