be3592f528c869673ecee5c7bd11933204968b6b
Valentin Décaillet <decaillv@gmail.com>
2023-03-13 15:23:11 +0100

    feat(ArgParser): parse preprocessor kwargs

--- src/arg_parser.py
+++ src/arg_parser.py
@@ -12,12 +12,24 @@ class ArgParser:
         parser.add_argument("--seed",
                             help="seed to lock random number generation",
                             type=int, default=None)
+
         parser.add_argument("--preprocessor-type",
                             help="type of preprocessor to use",
                             choices=PreprocessorFactory.allowed_types, default="standard")
+
+        help_polynomial_preprocessor_kwargs = (
+            "kwargs to pass to PolynomialFeatures. "
+            "Only valid if --preprocessor-type=polynomial. "
+            "Format: comma-separated 'field:position' pairs (e.g. 'degree: 2, interaction_only: False, include_bias: True, order: F'). "
+            "See https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.PolynomialFeatures.html for more details.")
+        parser.add_argument("--polynomial-preprocessor-kwargs",
+                            help=help_polynomial_preprocessor_kwargs,
+                            type=self._kwargs, default=None)
+
         parser.add_argument("--estimator-type",
                             help="type of estimator to use",
                             choices=EstimatorFactory.allowed_types, default="decision-tree")
+
         parser.add_argument("--evaluation-count",
                             help="number of times to evaluate the model",
                             type=self._strictly_positive_int, default=3)
@@ -25,7 +37,7 @@ class ArgParser:
         args = parser.parse_args(argv)
 
         self._seed = args.seed
-        self._preprocessor_factory = PreprocessorFactory(args.preprocessor_type)
+        self._preprocessor_factory = PreprocessorFactory(args.preprocessor_type, args.polynomial_preprocessor_kwargs)
         self._estimator_factory = EstimatorFactory(args.estimator_type)
         self._evaluation_count = args.evaluation_count
 
@@ -44,6 +56,22 @@ class ArgParser:
     @property
     def evaluation_count(self) -> int:
         return self._evaluation_count
+
+    @classmethod
+    def _kwargs(cls, str_value) -> dict:
+        try:
+            dict_value = {k.strip(): v.strip() for k, v in (i.split(':') for i in str_value.split(','))}
+            for k, v in dict_value.items():
+                if v == "True":
+                    dict_value[k] = True
+                elif v == "False":
+                    dict_value[k] = False
+                elif v.isdigit():
+                    dict_value[k] = int(v)
+            return dict_value
+        except ValueError:
+            pass
+        raise argparse.ArgumentTypeError(f"{str_value} is not a dictionary")
 
     @classmethod
     def _strictly_positive_int(cls, str_value) -> bool:
