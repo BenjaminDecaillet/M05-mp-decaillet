348b41e6649498115244e27232fe06aa09ffcd2f
Valentin Décaillet <decaillv@gmail.com>
2023-03-13 10:00:47 +0100

    test(estimator): cover

--- unit_tests/estimating/test_regression_estimator.py
+++ unit_tests/estimating/test_regression_estimator.py
@@ -6,20 +6,56 @@ from src.estimating import RegressionEstimator
 
 
 class TestRegressionEstimator(unittest.TestCase):
-    def test_fit(self):
+    def test__happy_path(self):
         estimator = RegressionEstimator()
-        features = pd.DataFrame({'x1': [1.0, 2.0, 3.0], 'x2': [4.0, 5.0, 6.0]})
-        targets = pd.DataFrame({'y1': [7.0, 8.0, 9.0]})
 
-        self.assertIsNone(estimator.fit(features, targets))
+        with self.subTest("fit"):
+            training_features = pd.DataFrame(data={
+                "feature 1": [1.0, 2.0, 3.0],
+                "feature 2": [4.0, 5.0, 6.0]
+            })
+            training_targets = pd.DataFrame(data={
+                "target 1": [7.0, 8.0, 9.0]
+            })
 
-    def test_predict(self):
+            actual = estimator.fit(training_features, training_targets)
+
+            self.assertIsNone(actual)
+
+        with self.subTest("predict"):
+            test_features = pd.DataFrame(data={
+                "feature 1": [4.0, 5.0, 6.0],
+                "feature 2": [7.0, 8.0, 9.0]
+            })
+
+            actual = estimator.predict(test_features)
+
+            pd.testing.assert_frame_equal(actual, pd.DataFrame(data={
+                "target 1": [9.0, 9.0, 9.0]
+            }))
+
+    def test__fails_it_not_fit(self):
         estimator = RegressionEstimator()
-        features = pd.DataFrame({'x1': [1.0, 2.0, 3.0], 'x2': [4.0, 5.0, 6.0]})
-        targets = pd.DataFrame({'y1': [7.0, 8.0, 9.0]})
-        test_features = pd.DataFrame({'x1': [4.0, 5.0, 6.0], 'x2': [7.0, 8.0, 9.0]})
-        estimator.fit(features, targets)
+        test_features = pd.DataFrame(data={"feature 1": [4.0, 5.0, 6.0]})
 
-        result = estimator.predict(test_features)
+        with self.assertRaisesRegex(RuntimeError, "^Estimator has not been fit yet.$"):
+            estimator.predict(test_features)
 
-        pd.testing.assert_frame_equal(result, pd.DataFrame({'y1': [9.0, 9.0, 9.0]}))
+    def test__fails_if_columns_dont_match(self):
+        estimator = RegressionEstimator()
+        training_features = pd.DataFrame(data={
+            "feature 1": [1.0, 2.0, 3.0],
+            "feature 2": [4.0, 5.0, 6.0]
+        })
+        training_targets = pd.DataFrame(data={
+            "target 1": [7.0, 8.0, 9.0]
+        })
+        test_features = pd.DataFrame(data={
+            "feature 1": [4.0, 5.0, 6.0],
+            "feature 3": [7.0, 8.0, 9.0]  # <-- different column name
+        })
+
+        estimator.fit(training_features, training_targets)
+
+        with self.assertRaisesRegex(ValueError, "^Features have different columns than training features.$"):
+            estimator.predict(test_features)
